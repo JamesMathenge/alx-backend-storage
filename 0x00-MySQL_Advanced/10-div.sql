a73ac18dc59590aa978a93b2baf4c52b41a56dea
James Mathenge <mathengejames64@gmail.com>
2024-01-18 22:46:01 +0300

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,20 +5,18 @@
 --     a, INT
 --     b, INT
 -- - And returns a / b or 0 if b == 0
-DELIMITER //
-
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 4)
+-- creates a function SafeDiv that divides (and returns) the first
+-- by the second number or returns 0 if the second number is equal to 0.
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLARE result DECIMAL(10, 4);
+    DECLARE result FLOAT DEFAULT 0;
 
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
+    IF b != 0 THEN
         SET result = a / b;
     END IF;
-
     RETURN result;
-END //
-
+END $$
 DELIMITER ;
